b06373fe1234cccb45ba2a6b6664153c3c4564ba
Tyler <tylermcrae480@gmail.com>
2025-08-28 21:18:40 -0400

    remove more files

---
+++ pages/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  entry: "./pages/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      { test: /.m?js$/, exclude: /node_modules/, use: "babel-loader" },
+      { test: /.css$/, use: ["style-loader", "css-loader"] },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "/index.html" })],
+  devServer: { static: "./dist", open: true },
+}
